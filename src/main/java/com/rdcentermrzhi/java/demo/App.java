ee191f0fcc348bce7d0217dda415b8c6ee0c75f8
whoami <2675204902@qq.com>
2018-07-31 10:24:59 +0800

    初始化仓库

---
+++ src/main/java/com/rdcentermrzhi/java/demo/App.java
@@ -0,0 +1,13 @@
+package com.rdcentermrzhi.java.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
