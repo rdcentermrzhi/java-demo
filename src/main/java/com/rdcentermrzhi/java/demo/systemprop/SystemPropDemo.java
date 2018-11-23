dec540072e17373b76a4a841fff0ac287fd19085
whoami <2675204902@qq.com>
2018-11-23 16:31:45 +0800

    获取java系统参数

---
+++ src/main/java/com/rdcentermrzhi/java/demo/systemprop/SystemPropDemo.java
@@ -0,0 +1,12 @@
+package com.rdcentermrzhi.java.demo.systemprop;
+
+public class SystemPropDemo {
+
+	public static void main(String[] args) {
+		
+		System.out.println(System.getProperty("java.class.path"));
+		
+		
+		
+	}
+}
